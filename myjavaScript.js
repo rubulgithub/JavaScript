98c9a19ebf9ea25b1681687ca26d2c57dfef6f04
rubulgithub <114800261+rubulgithub@users.noreply.github.com>
2023-10-03 11:28:11 +0530

    first commit

---
+++ myjavaScript.js
@@ -0,0 +1,11 @@
+function fandom() {
+  let pass = "";
+  let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  // if (numAllowed) str += "0123456789";
+  // if (charAllowed) str += "!@#$%^&*()-_=+[]{}~`";
+
+  for (let i = 1; i <= length; i++) {
+    let char = Math.floor(Math.random() * str.length + 1);
+    console.log(char);
+  }
+}
